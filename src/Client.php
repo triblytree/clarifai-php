9cde45d10e9ef948d8f663ef3121969c8ca68c06
triblytree <charliekonsor@gmail.com>
2018-10-17 16:04:03 -0700

    Update Client.php

--- src/Client.php
+++ src/Client.php
@@ -90,14 +90,13 @@ class Client
      */
     public function accessToken()
     {
-        $response = $this->client->send($this->client->createRequest('POST', $this->baseUrl . '/token', [
+        $response = $this->client->request('POST', $this->baseUrl . '/token', [
             'headers' => [
                 'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
             ],
             'auth' => [$this->clientId, $this->clientSecret],
-        ]));
-
+        ]);
         return json_decode($response->getBody(), true);
     }
 }
